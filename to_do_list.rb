b91678bd316c9486df5356df91f2c3f21cd98c15
danielboyle <danielboyle111@gmail.com>
2015-12-15 15:55:42 -0500

    Initial run through of Todo list

--- to_do_list.rb
+++ to_do_list.rb
@@ -1,1 +1,92 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
 
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title, :todos
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def <<(todo)
+    raise TypeError, "can only add Todo objects" unless todo.instance_of? Todo
+
+    todos << todo
+  end
+  alias_method :add, :<<
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def item_at(idx)
+    todos.fetch(idx)
+  end
+
+  def mark_done_at(idx)
+    item_at(idx).done!
+  end
+
+  def mark_undone_at(idx)
+    item_at(idx).undone!    
+  end
+
+  def remove_at(idx)
+    todos.delete(item_at(idx))    
+  end
+
+  def done!
+    todos.each_index do |idx|
+      mark_done_at(idx)
+    end
+  end
+end
